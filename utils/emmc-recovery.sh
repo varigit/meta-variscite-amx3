504d57b495943d4e9df1e9fc6102aea03b96c7e0
Felix Radensky <felix.r@variscite.com>
2016-10-20 15:08:58 +0300

    1. Adapted to new sfdisk option syntax 2. Make sure that ext3 partition is not mounted before format 3. Removed tar verbose option 4. Unmount both eMMC partitions before exit

--- utils/emmc-recovery.sh
+++ utils/emmc-recovery.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-umount /run/media/mmcblk1p1
-umount /run/media/mmcblk1p2
+umount /dev/mmcblk1p1 >& /dev/null
+umount /dev/mmcblk1p2 >& /dev/null
 
-sfdisk -D -H 255 -S 63 -C 950 /dev/mmcblk1 << EOF
-,9,0x0C,*
-10,,,-
+# Create partition table 
+# 2 partitions: FAT32 (70MB) and Linux ext3 (remaining space)
+sfdisk /dev/mmcblk1 << EOF
+,70Mib,0x0C,*
+,,,-
 EOF
 
 sync 
 
-umount /run/media/mmcblk1p1
-umount /run/media/mmcblk1p2
-
+# Format boot partition
+umount /dev/mmcblk1p1 >& /dev/null
 mkfs.vfat -F 32 -n "boot" /dev/mmcblk1p1
-mkfs.ext3 -L "rootfs" /dev/mmcblk1p2
-
 sync
 
+# Format rootfs partition
+umount /dev/mmcblk1p2 >& /dev/null
+dd if=/dev/zero of=/dev/mmcblk1p2 bs=128K count=1
+mkfs.ext3 -L "rootfs" /dev/mmcblk1p2
+sync
+                                                                                        
+# Mount boot partition
+umount /dev/mmcblk1p1 >& /dev/null
 rm -rf /run/media/mmcblk1p1
 mkdir /run/media/mmcblk1p1
 mount /dev/mmcblk1p1 /run/media/mmcblk1p1
 
+# Mount rootfs partition
+umount /dev/mmcblk1p2 >& /dev/null 
 rm -rf /run/media/mmcblk1p2
 mkdir /run/media/mmcblk1p2
 mount /dev/mmcblk1p2 /run/media/mmcblk1p2
@@ -33,9 +42,12 @@ cp /opt/TISDK/emmc/MLO /run/media/mmcblk1p1
 cp /opt/TISDK/emmc/u-boot.img /run/media/mmcblk1p1
 
 echo "Extracting rootfs to the EMMC device"
-tar xvf /opt/TISDK/tisdk-rootfs-image-varsomamx3.tar.gz -C /run/media/mmcblk1p2
+tar xf /opt/TISDK/tisdk-rootfs-image-varsomamx3.tar.gz -C /run/media/mmcblk1p2
 
 # prevent touch screen calibration
 touch /run/media/mmcblk1p2/etc/udev/rules.d/ws-calibrate.rules
 
 sync
+
+umount /dev/mmcblk1p1
+umount /dev/mmcblk1p2
